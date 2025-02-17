06af5d4e6aea43dd17b9e529fc7e0966db755999
Fauzan Pradana <fauzanpr.06@gmail.com>
2025-02-17 15:33:35 +0700

    initial commit

---
+++ resources/views/link.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <h1>Data Link</h1>
+    <table>
+        <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>Category</th>
+            <th></th>
+        </tr>
+        <tr>
+            <th>1</th>
+            <th>Link LMS</th>
+            <th>Kuliah</th>
+            <th></th>
+        </tr>
+    </table>
+</body>
+
+</html>
